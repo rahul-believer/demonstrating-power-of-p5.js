ce99f6eb2ab705bfe3a9b92e93a58beee7347fc8
Rahul singh chauhan <31926797+rahul-believer@users.noreply.github.com>
2020-06-29 13:12:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,49 @@
-let canvas;
-
 function setup() {
-	canvas = createCanvas(windowWidth, windowHeight);
-	textFont('Roboto Mono');
+  createCanvas(400, 400);
+  angleMode(DEGREES);
 }
 
 function draw() {
-	background(0);
-	
-	stroke(255);
-	noFill();
-	
-	let now = new Date();
-	
-	let mil = now.getMilliseconds();
-	let sec = now.getSeconds() + mil / 1000;
-	let s = sec / 60;
-	let min = now.getMinutes() + s;
-	let m = min / 60;
-	let hou = now.getHours() % 12 + m;
-	let h = hou / 12;
-	
-	translate(width / 2, height / 2);
-	
-	let x = Math.min(width, height) * 0.9;
-	let x2 = x * 0.9;
-	let x3 = x * 0.8;
-	
-	strokeCap(SQUARE);
-	strokeWeight(30 / 1000 * x);
-	textAlign(CENTER, CENTER);
-	
-	push();
-	rotate(-HALF_PI);
-	
-	arc(0, 0, x, x, 0, h * TAU);
-	arc(0, 0, x2, x2, 0, m * TAU);
-	arc(0, 0, x3, x3, 0, s * TAU);
-	pop();
-	
-	noStroke();
-	fill(255);
-	
-	textSize(100 / 1000 * x);
-	let clockText = [
-			floor(hou),
-			floor(min),
-			floor(sec)
-		]
-		.map(n => ('0' + n).slice(-2))
-		.join(':');
-	text(clockText, 0, 0);
-}
+  background(0);
+  translate(200, 200);
+  rotate(-90);
 
-function windowResized() {
-	resizeCanvas(windowWidth, windowHeight);
+  let hr = hour();
+  let mn = minute();
+  let sc = second();
+
+  strokeWeight(8);
+  stroke(255, 100, 150);
+  noFill();
+  let secondAngle = map(sc, 0, 60, 0, 360);
+  arc(0, 0, 300, 300, 0, secondAngle);
+
+  stroke(150, 100, 255);
+  let minuteAngle = map(mn, 0, 60, 0, 360);
+  arc(0, 0, 280, 280, 0, minuteAngle);
+
+  stroke(150, 255, 100);
+  let hourAngle = map(hr % 12, 0, 12, 0, 360);
+  arc(0, 0, 260, 260, 0, hourAngle);
+
+  push();
+  rotate(secondAngle);
+  stroke(255, 100, 150);
+  line(0, 0, 100, 0);
+  pop();
+
+  push();
+  rotate(minuteAngle);
+  stroke(150, 100, 255);
+  line(0, 0, 75, 0);
+  pop();
+
+  push();
+  rotate(hourAngle);
+  stroke(150, 255, 100);
+  line(0, 0, 50, 0);
+  pop();
+
+  stroke(255);
+  point(0, 0);
 }
